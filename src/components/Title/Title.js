cd911043b6d44984c5d30bfa683a640950dfe80a
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2021-11-26 23:12:03 -0600

    Decoracion de texto para los enlaces

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,17 +2,15 @@ import { Link } from 'react-router-dom';
 import './Title.css';
 
 const Title = () => (
-    <section className="title" >
-        <Link to="/">
-        <img className="tittle-imagen" src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/icons/Icon_TipoWhite.png" alt="logo">
-            
-            </img> 
+    <Link to="/">
+        <section className="title" >
+            <img className="tittle-imagen" src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/icons/Icon_TipoWhite.png" alt="logo">
+            </img>
             <p className="tittle-parrafo" >
                 MTV Music Awards 2021
             </p>
-        </Link>
- 
-    </section>
+        </section>
+    </Link>
 )
 
 export default Title;
