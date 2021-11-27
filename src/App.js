fc5ec9998a087a4cea2b82687d6d5031200bee24
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2021-11-26 22:26:35 -0600

    Declaracion de las rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import  Home  from "../src/components/Pages/Home/Home.js";
-import  Vote  from "../src/components/Pages/Vote/Vote";
-import logo from './logo.svg';
+import  Vote  from "../src/components/Pages/Vote/Vote.js";
+import SignIn from "./components/Pages/SignIn/SignIn.js";
+import Login from "./components/Pages/Login/Login.js";
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-import WarningVote from "./components/WarningVote/WarningVote.js";
 
 function App() {
   return (
-    <Home/>    
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/"  element={<Home/>} />
+        <Route exact path="/users/signin"  element={<SignIn/>} />
+        <Route exact path="/users/login"  element={<Login/>} />
+        <Route exact path="/users/vote"  element={<Vote/>} />
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
