06a9606168d716cbafbbc134d6b8db1bfc20a67b
davidnolasco <davidnolasco544@gmail.com>
2021-12-04 20:45:47 -0600

    Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import './Modal.css';
+
+
+
+const Modal = () => {
+    return (
+        <>
+            <section>
+                <p>Multiverso Confirmado</p>
+                <p>¡Creaste tu Cuenta Sherli.2!</p>
+            </section>
+        </>
+    )
+}
+
+export default Modal;
