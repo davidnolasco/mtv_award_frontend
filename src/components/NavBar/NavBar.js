58dd17f0d1b8b7a61f39439322496411598870f4
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2021-12-05 21:23:48 -0600

    Validacion de usario en navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
 import './NavBar.css';
+import { useSelector } from "react-redux";
+const getSecurity = ({ security }) => security;
 
 
+const NavBar = () => {
+    const security = useSelector(getSecurity);
+    console.log(security)
 
-const NavBar = ({ typeNav = "Bar", email }) => {
     return (
         <>
             {
-                typeNav === "Bar" ?
-                    <nav className="navbar" >
-                        <Link to="/users/login">  <p className="navbar-boton" >Iniciar sesión</p></Link>                      
-                        <Link to="/users/signin"> <p className="navbar-boton navbar-boton--primary" >Crear Cuenta</p></Link>
-                       
-                    </nav>
+                security.user.email ?
 
+                    <nav className="navbar" >
+                        <img src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/icons/StarIcon.png" alt="Avatar" />
+                        <p >{security.user.email.split('@')[0]}</p>
+                    </nav>
 
                     :
                     <nav className="navbar" >
-                        <p >{email}</p>
-                        <img src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/icons/StarIcon.png" alt="Avatar" />
+                        <Link to="/users/login">  <p className="navbar-boton" >Iniciar sesión</p></Link>
+                        <Link to="/users/signin"> <p className="navbar-boton navbar-boton--primary" >Crear Cuenta</p></Link>
+
                     </nav>
             }
         </>
