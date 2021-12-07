1b0be4ba3453fa9df4e29deb8f0e5d926301205e
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2021-12-07 13:30:40 -0600

    Cerrar sesion y validacion de usuario en main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import './Main.css';
 
-const Main = () => (
+const getSecurity = ({ security }) => security;
+
+const Main = () => {
+    const security = useSelector(getSecurity);
+    const votes = "/users/vote";
+    const iniciarSesion = "/users/login";
+    
+    return(
     <section className="main-container">
         <div className="main-content">
             <img className="main-imagen" src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/images/DonaMtv.png" alt="Logo MTV"></img>
-            <Link to="/users/vote"> <p className="main-boton" >Votar ahora</p></Link>
+            <Link to={security.user.email ? votes : iniciarSesion}> <p className="main-boton" >Votar ahora</p></Link>
         </div>
     </section>
-)
+)}
 
 export default Main;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
-import './NavBar.css';
 import { useSelector } from "react-redux";
+import './NavBar.css';
+import { useState } from "react";
 const getSecurity = ({ security }) => security;
 
 
 const NavBar = () => {
     const security = useSelector(getSecurity);
-    console.log(security)
+    
+    const [active, setActive] = useState(false);
+
+    const onClickNavBar =()=>{
+        setActive(true)
+    }
+
+    const onClickCloseSesion =()=>{
+        setActive(false);
+        localStorage.clear();
+        window.location.replace('/');
+    }
 
     return (
         <>
             {
                 security.user.email ?
-
-                    <nav className="navbar" >
+                    <nav>
+                    <div className="navbar" onClick={onClickNavBar}>
                         <img src="https://raw.githubusercontent.com/gustavo-exe/MTV_Award_Files/main/icons/StarIcon.png" alt="Avatar" />
                         <p >{security.user.email.split('@')[0]}</p>
+                    </div>
+                    <div  onClick={onClickCloseSesion} className={`${active ? "navbar-closesesion navbar-closesesion--active" : "navbar-closesesion"}`} >
+                        Cerrar sesion
+                    </div>
                     </nav>
-
                     :
                     <nav className="navbar" >
                         <Link to="/users/login">  <p className="navbar-boton" >Iniciar sesión</p></Link>
